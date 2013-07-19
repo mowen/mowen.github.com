1266e9a8e26683bc71bc251617ea7369695b816f
Martin Owen <martinowenuk@gmail.com>
2013-07-19 20:31:12 +0100

    Initial commit.

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/*
+Author: Martin Owen
+*/
+
+
+(function() {
+  $(function() {
+    var options;
+
+    return options = {
+      "in": {
+        effect: 'tada',
+        shuffle: true
+      },
+      out: {
+        effect: 'tada',
+        shuffle: true
+      },
+      loop: true
+    };
+  });
+
+}).call(this);
